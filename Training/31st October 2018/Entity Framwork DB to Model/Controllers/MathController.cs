ec8a959b561d8c531313b3f7dfcc6c9c0861ac30
vishal1508 <vkvishal1508@gmail.com>
2018-11-06 09:06:06 +0530

    Add files via upload

---
+++ Training/31st October 2018/Entity Framwork DB to Model/Controllers/MathController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Example.Models;
+
+namespace MVC_Example.Controllers
+{
+    public class MathController : Controller
+    {
+        // GET: Math
+        public ActionResult Index()
+        { 
+            return View();
+        }
+
+        public ActionResult Result(MathOperation mop)
+        {
+            mop.FindResult();
+            MathOperation.ilist.Add(mop);
+            return View("Index",MathOperation.ilist);
+        }
+    }
+}

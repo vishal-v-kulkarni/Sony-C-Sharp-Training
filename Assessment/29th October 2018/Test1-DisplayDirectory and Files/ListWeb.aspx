2641724c021f132b6b925ed4d56c34fb132e1b7e
vishal1508 <vkvishal1508@gmail.com>
2018-11-06 09:06:41 +0530

    Add files via upload

---
+++ Assessment/29th October 2018/Test1-DisplayDirectory and Files/ListWeb.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ListWeb.aspx.cs" Inherits="Test1_DisplayDirectory.ListWeb" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        #TextArea1 {
+            height: 276px;
+            width: 801px;
+        }
+        #text {
+            height: 146px;
+            width: 875px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:DropDownList ID="listdrives" runat="server" AutoPostBack="True" OnSelectedIndexChanged="listdrives_SelectedIndexChanged">
+            </asp:DropDownList>
+        </div>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:ListBox ID="listbox" runat="server" AutoPostBack="True" Height="216px" OnSelectedIndexChanged="listbox_SelectedIndexChanged" Width="607px"></asp:ListBox>
+        <p>
+            <textarea runat="server" id="TextArea1" name="S1"></textarea></p>
+    </form>
+</body>
+</html>
---
+++ Assessment/29th October 2018/Test1-DisplayDirectory and Files/ListWeb.aspx.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Test1_DisplayDirectory
+{
+    public partial class ListWeb : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                DriveInfo[] drives = DriveInfo.GetDrives();
+
+                DropDownList drop = (DropDownList)FindControl("listdrives");
+
+                foreach (var a in drives)
+                {
+                    ListItem li = new ListItem();
+                    li.Text = a.ToString();
+
+                    drop.Items.Add(li);
+                }
+
+                string[] directories = Directory.GetDirectories(listdrives.Text);
+
+                ListBox lb = (ListBox)FindControl("listbox");
+                lb.Items.Clear();
+
+                foreach (var a in directories)
+                {
+                    ListItem li = new ListItem();
+                    li.Text = a.ToString();
+
+                    lb.Items.Add(li);
+                }
+            }
+
+
+
+        }
+
+        protected void listdrives_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            string[] directories = Directory.GetDirectories(listdrives.Text);
+
+            ListBox lb = (ListBox)FindControl("listbox");
+            lb.Items.Clear();
+
+            foreach (var a in directories)
+            {
+                ListItem li = new ListItem();
+                li.Text = a.ToString();
+
+                lb.Items.Add(li);
+            }
+
+        }
+
+        protected void listbox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            ListBox lb = (ListBox)FindControl("listbox");
+
+            FileAttributes attr = File.GetAttributes(lb.SelectedItem.Text);
+            //Response.Write(lb.SelectedItem.Text);
+
+            if ((attr & FileAttributes.Directory) == FileAttributes.Directory)
+            {
+                string[] directories = Directory.GetDirectories(lb.SelectedItem.Text);
+
+                DirectoryInfo d = new DirectoryInfo(lb.SelectedItem.Text);
+                FileInfo[] files = d.GetFiles("*.cs");
+
+                lb.Items.Clear();
+
+
+                foreach (var a in directories)
+                {
+                    ListItem li = new ListItem();
+                    li.Text = a.ToString();
+
+                    lb.Items.Add(li);
+                }
+                foreach (var f in files)
+                {
+                    ListItem li = new ListItem();
+                    li.Text = d.FullName+"\\"+f.ToString();
+
+                    lb.Items.Add(li);
+                }
+            }
+            else
+            {
+                FileInfo f = new FileInfo(lb.SelectedItem.Text);
+                string fullpath = Path.GetFullPath(lb.SelectedItem.Text);
+
+                string contents = File.ReadAllText(fullpath);
+
+
+                TextArea1.InnerText = contents;
+
+                Process.Start("notepad.exe",fullpath);
+
+
+            }
+
+           
+
+            
+
+        }
+    }
+}
